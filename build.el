f21bbf129c90cc9e2b4d152b56631912181a936b
onlyvip <chawhanvip@gmail.com>
2026-01-25 20:23:28 +0530

    unchanged

--- build.el
+++ build.el
@@ -3,55 +3,46 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
+;; Initialize package system
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; Ensure org is available
 (unless (package-installed-p 'org)
   (package-install 'org))
 
+;; Load org and org-id
 (require 'org)
 (require 'org-id)
 (require 'ox-publish)
 
-;; Org-roam setup
+;; Set org-roam directory for batch mode
 (setq org-roam-directory "./notes")
+
+;; Build org-id database from all files in notes/
 (setq org-id-track-globally t)
 (setq org-id-locations-file "./.org-id-locations")
 
-;; Build ID database
+;; Scan notes directory and build ID database
 (org-id-update-id-locations 
  (directory-files-recursively "./notes" "\\.org$"))
 
-;; === PERFECT SITEMAP FUNCTION ===
-(defun my/org-simple-sitemap (title sitemap)
-  "Generate clean hierarchical sitemap from TITLE and SMAP (list of strings)."
-  (with-temp-buffer
-    (insert (format "* %s\n\n** All Notes\n" title))
-    ;; Sort filenames alphabetically (extract from [[file:xxx][title]])
-    (let ((entries (sort sitemap 
-                         (lambda (a b) 
-                           (string< (my/extract-filename a) 
-                                    (my/extract-filename b))))))
-      (dolist (entry entries)
-        (let ((filename (my/extract-filename entry))
-              (title-str (my/extract-title entry)))
-          (insert (format "- [[file:%s][%s]]\n" filename title-str)))))
-    (buffer-string)))
+;; === CUSTOM FUNCTIONS (MUST COME FIRST) ===
+(defun my/org-simple-sitemap (project &optional opts)
+  "Simple sitemap with titles."
+  (let ((files (org-publish-get-base-files project opts)))
+    (with-temp-buffer
+      (insert "* Knowledge Base\n\n")
+      (insert "** Notes\n")
+      (dolist (file (sort files #'string<))
+        (let ((title (org-publish-find-title file project)))
+          (insert (format "- [[file:%s][%s]]\n" 
+                          (file-name-sans-extension (file-name-nondirectory file))
+                          title))))
+      (buffer-string))))
 
-(defun my/extract-filename (entry)
-  "Extract filename from [[file:filename][title]] string."
-  (if (string-match "\\[\\[file:([^]]+)\\]\\[" entry)
-      (match-string 1 entry)
-    (file-name-sans-extension (file-name-nondirectory entry))))
-
-(defun my/extract-title (entry)
-  "Extract title from [[file:filename][title]] string."
-  (if (string-match "\\]\\[\\([^]]+\\)\\]\\]" entry)
-      (match-string 1 entry)
-    "Untitled"))
-
-;; === PUBLISHING PROJECTS ===
+;; === PUBLISHING CONFIGURATION ===
 (setq org-publish-project-alist
       '(("org-roam-notes"
          :base-directory "./notes"
@@ -63,13 +54,13 @@
          :auto-sitemap t
          :sitemap-filename "index.org"
          :sitemap-title "Knowledge Base"
-         :sitemap-function my/org-simple-sitemap
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\">
-                     <link rel=\"stylesheet\" type=\"text/css\" href=\"gray-theme.css\">"
+         :sitemap-sort-files anti-chronologically
+         :sitemap-function my/org-simple-sitemap    ;; ← ADD THIS LINE
+         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\"/>"
          :html-head-include-default-style nil
          :html-link-org-files-as-html t
          :section-numbers nil
-         :with-toc nil
+         :with-toc t
          :html-postamble nil)
         ("static"
          :base-directory "./notes"
@@ -80,6 +71,4 @@
         ("website" :components ("org-roam-notes" "static"))))
 
 (setq org-html-head-include-default-style nil)
-
-;; Publish everything!
-(org-publish "website" t)
+(setq org-html-head-include-scripts nil)
