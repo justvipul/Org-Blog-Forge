fa9423cc60ebec9b031446e53b6b45ee0aaed26a
onlyvip <chawhanvip@gmail.com>
2026-01-25 20:16:54 +0530

    done

--- build.el
+++ build.el
@@ -3,46 +3,39 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Initialize package system
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Ensure org is available
 (unless (package-installed-p 'org)
   (package-install 'org))
 
-;; Load org and org-id
 (require 'org)
 (require 'org-id)
 (require 'ox-publish)
 
-;; Set org-roam directory for batch mode
+;; Set org-roam directory
 (setq org-roam-directory "./notes")
-
-;; Build org-id database from all files in notes/
 (setq org-id-track-globally t)
 (setq org-id-locations-file "./.org-id-locations")
 
-;; Scan notes directory and build ID database
+;; Build ID database
 (org-id-update-id-locations 
  (directory-files-recursively "./notes" "\\.org$"))
 
-;; === CUSTOM FUNCTIONS (MUST COME FIRST) ===
-(defun my/org-simple-sitemap (project &optional opts)
-  "Simple sitemap with titles."
-  (let ((files (org-publish-get-base-files project opts)))
-    (with-temp-buffer
-      (insert "* Knowledge Base\n\n")
-      (insert "** Notes\n")
-      (dolist (file (sort files #'string<))
-        (let ((title (org-publish-find-title file project)))
-          (insert (format "- [[file:%s][%s]]\n" 
-                          (file-name-sans-extension (file-name-nondirectory file))
-                          title))))
-      (buffer-string))))
+;; === FIXED SITEMAP FUNCTION ===
+(defun my/org-simple-sitemap (title sitemap)
+  "Generate simple hierarchical sitemap. TITLE is sitemap title, SMAP is list of files."
+  (with-temp-buffer
+    (insert (format "* %s\n\n** Notes\n" title))
+    (dolist (entry (sort sitemap #'string<))
+      (let* ((file (car entry))
+             (filename (file-name-sans-extension (file-name-nondirectory file)))
+             (title (or (cdr entry) filename)))
+        (insert (format "- [[file:%s][%s]]\n" filename title))))
+    (buffer-string)))
 
-;; === PUBLISHING CONFIGURATION ===
+;; === PUBLISHING PROJECTS ===
 (setq org-publish-project-alist
       '(("org-roam-notes"
          :base-directory "./notes"
@@ -54,13 +47,13 @@
          :auto-sitemap t
          :sitemap-filename "index.org"
          :sitemap-title "Knowledge Base"
-         :sitemap-sort-files anti-chronologically
-         :sitemap-function my/org-simple-sitemap    ;; ← ADD THIS LINE
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\"/>"
+         :sitemap-function my/org-simple-sitemap  ;; ✅ FIXED
+         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\"/>
+                     <link rel=\"stylesheet\" type=\"text/css\" href=\"gray-theme.css\"/>"
          :html-head-include-default-style nil
          :html-link-org-files-as-html t
          :section-numbers nil
-         :with-toc t
+         :with-toc nil  ;; Disable TOC for cleaner index
          :html-postamble nil)
         ("static"
          :base-directory "./notes"
@@ -72,3 +65,6 @@
 
 (setq org-html-head-include-default-style nil)
 (setq org-html-head-include-scripts nil)
+
+;; Publish!
+(org-publish "website" t)
