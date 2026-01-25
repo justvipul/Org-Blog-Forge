1aa9ab5a9daa2775fac8fb9e96b952f289e7c01d
onlyvip <chawhanvip@gmail.com>
2026-01-25 20:25:34 +0530

    check

--- build.el
+++ build.el
@@ -28,47 +28,35 @@
 (org-id-update-id-locations 
  (directory-files-recursively "./notes" "\\.org$"))
 
-;; === CUSTOM FUNCTIONS (MUST COME FIRST) ===
-(defun my/org-simple-sitemap (project &optional opts)
-  "Simple sitemap with titles."
-  (let ((files (org-publish-get-base-files project opts)))
-    (with-temp-buffer
-      (insert "* Knowledge Base\n\n")
-      (insert "** Notes\n")
-      (dolist (file (sort files #'string<))
-        (let ((title (org-publish-find-title file project)))
-          (insert (format "- [[file:%s][%s]]\n" 
-                          (file-name-sans-extension (file-name-nondirectory file))
-                          title))))
-      (buffer-string))))
-
-;; === PUBLISHING CONFIGURATION ===
+;; Publishing configuration
 (setq org-publish-project-alist
       '(("org-roam-notes"
-         :base-directory "./notes"
-         :base-extension "org"
-         :publishing-directory "./public"
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :auto-sitemap t
-         :sitemap-filename "index.org"
-         :sitemap-title "Knowledge Base"
-         :sitemap-sort-files anti-chronologically
-         :sitemap-function my/org-simple-sitemap    ;; ← ADD THIS LINE
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\"/>"
-         :html-head-include-default-style nil
-         :html-link-org-files-as-html t
-         :section-numbers nil
-         :with-toc t
-         :html-postamble nil)
+	 :base-directory "./notes"
+	 :base-extension "org"
+	 :publishing-directory "./public"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4
+	 :auto-sitemap t
+	 :sitemap-filename "index.org"
+	 :sitemap-title "Knowledge Base"
+	 :sitemap-sort-files anti-chronologically
+	 :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"index-style.css\"/>"
+	 :html-head-include-default-style nil
+	 :html-link-org-files-as-html t
+	 :section-numbers nil
+	 :with-toc t
+	 :html-postamble nil)
         ("static"
          :base-directory "./notes"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|svg"
          :publishing-directory "./public"
          :recursive t
          :publishing-function org-publish-attachment)
-        ("website" :components ("org-roam-notes" "static"))))
-
+     
+        ;; Update this to include RSS
+        ("website" :components ("org-roam-notes" "static"))
+        ))
+	
 (setq org-html-head-include-default-style nil)
 (setq org-html-head-include-scripts nil)
